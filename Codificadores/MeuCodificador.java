007fea2eba707824afe76ccdcd00054ec3e3dc05
Giovanni Schenato <gischenato@gmail.com>
2022-08-16 10:53:16 -0300

    Corrigindo bugs e simplificando

--- Codificadores/MeuCodificador.java
+++ Codificadores/MeuCodificador.java
@@ -1,6 +1,5 @@
 package Codificadores;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class MeuCodificador implements Codifica{
@@ -19,66 +18,32 @@ public class MeuCodificador implements Codifica{
      public String codifica(String str) {
           Random rand = new Random();
           String codificado = "";
-          int pos = -1;
-          for(Byte b : str.getBytes()){
-               pos++;
-               if(b < 0){
-                    codificado += str.substring(pos, pos+1);
-                    continue;
+          for(Character c : str.toCharArray()){
+               for(char binario : Integer.toString(c, 2).toCharArray()){
+                    for(int i = 0; i < rand.nextInt(4); i++)
+                         codificado += (rand.nextInt(6) + 2);
+                    codificado += binario; 
+                    for(int i = 0; i < rand.nextInt(1); i++)
+                         codificado += (rand.nextInt(6) + 2);
                }
-               String bin = Integer.toBinaryString(b);
-               String newBin = "";
-               for(char s : bin.toCharArray()){
-                    String val = "" + (rand.nextInt(6) + 2);
-                    newBin += s+val;
-               }
-               codificado+=newBin + "9";
+               codificado+="9";
           }
           return codificado;
      }
 
      @Override
      public String decodifica(String str) {
-          ArrayList<ArrayList<String>> bins2 = new ArrayList<>();
-          ArrayList<String> bins = new ArrayList<>();
-          String bin = "";
-          String t = "";
-          for(char v : str.toCharArray()){
-               t+=v;
-               if(v == '0' || v == '1') bin += v;
-               else if(v == '9'){
-                    bins.add(bin);
-                    System.out.println("Teta");
-                    System.out.println(bin);
-                    bin = "";
-               }  
-               else if(Character.isLetter(v)){
-                    System.out.println("TO QUI");
-                    System.out.println(bin);
-                    if(bins.size() > 0)bins2.add(bins);
-                    ArrayList<String> temp = new ArrayList<>();
-                    bin += v;
-                    temp.add(bin);
-                    bins2.add(temp);
-                    bins = new ArrayList<>();
-                    bin = "";
-               }
-          }
-          bins2.add(bins);
-          System.out.println(t.replaceAll("[2-8]", "").replace('9', ' '));
-
           String original = "";
-          for (ArrayList<String> a : bins2) {
-               System.out.println(a);
-               if(Character.isDigit(a.get(0).charAt(0)))
-                    original += a.stream()
-                              .map(binario -> Integer.parseInt(binario, 2))
-                              .map(value -> Character.toString(value))
-                              .reduce("", (c1, c2) -> c1 + c2);
-               else
-                    for(String s : a) original+=s;
+          String aux = "";
+          for(char c : str.toCharArray()){
+               if(c == '9'){
+                    original += (char) Integer.parseInt(aux, 2);
+                    aux = "";
+                    continue;
+               }
+               else if (c == '0' || c == '1')
+                    aux += c;
           }
-
           return original;
      }     
 }
