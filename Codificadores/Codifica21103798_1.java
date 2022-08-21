8d4dc2269998271f87f8e6c3cbf16a3b5123f80d
Eduardo M. Martins <martins-dudu@hotmail.com>
2022-08-21 13:23:22 -0300

    nome e matricula

---
+++ Codificadores/Codifica21103798_1.java
@@ -0,0 +1,25 @@
+package Codificadores;
+
+public class Codifica21103798_1 implements Codifica {
+ 
+    @Override
+    public String codifica(String str) {
+        return str;
+    }
+
+    @Override
+    public String decodifica(String str) {
+       
+        return str;
+    }
+
+    @Override
+    public String getMatriculaAutor() {
+        return "21103798-1";
+    }
+
+    @Override
+    public String getNomeAutor() {
+        return "Eduardo Machado Martins";
+    }
+}
