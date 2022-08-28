a306b64ef91e90aa9428b5b3da17fb57183dba78
gKuticoski <guilherme.kuticoski@edu.pucrs.br>
2022-08-27 23:50:04 -0300

    Adiciona arrays

---
+++ Codificadores/Codifica21101103.java
@@ -0,0 +1,36 @@
+package Codificadores;
+
+public class Codifica032614 implements Codifica {
+    private static char[] jogadoresTimao = {
+        "Rafael Ramos", "Bambu", "Gil", "Maycon", "Piton", "Ranato Augusto", "Yuri Alberto", "Roger Guedes", 
+        "Giuliano", "Cassio", "Paulinho(Nem lembrava que ainda esta lá)", "Ramiro", "Junior Moraes", "Mosquito",
+        "Mateus Vital", "Carlos Miguel", "Fagner", "Cantillo", "Fabio Santos", "Bruno Mendez", "Bruno Melo", "Adson", "Roni",
+        "Balbuena", "Matheus Doneli", "Fausto Vera", "Raul Gustavo", "Ruan Oliveira", "Du Queiroz", "Leo Natel", "Xavier"
+    };
+    private static char[] encontrosComInter = {
+        "Tinga simulando penalti", "DVD", "Brasileiro 2009", "Copa do Brasil 2009", "Edenilsoooon 41 anos", "Yuri Alberto"
+    };
+
+    @Override
+    public String codifica(String str) {
+        String codificada = "";
+        return codificada;
+    }
+
+    @Override
+    public String decodifica(String str) {
+        String decodificada = "";
+        return decodificada;
+    }
+
+    @Override
+    public String getMatriculaAutor() {
+        return "21101103-6";
+    }
+
+    @Override
+    public String getNomeAutor() {
+        return "Guilherme Romanini";
+    }
+
+}
