a00f762182c5041bcd294291911ac027a78f4fb3
Eduardo Balzan <eduardojbalzan@hotmail.com>
2022-08-28 12:31:07 -0300

    implementacao da classe Codifica21100417

--- Codificadores/Codifica21100417.java
+++ Codificadores/Codifica21100417.java
@@ -1,15 +1,32 @@
 package Codificadores;
 
+import java.util.Random;
+
 public class Codifica21100417 implements Codifica {
+    private static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
 
     @Override
     public String codifica(String str) {
-        return new StringBuilder(str).reverse().toString();
+        String codificada = "";
+        Random r = new Random();
+        // adiciona um caracter gerado aleatoriamente ao lado direito
+        // de cada caracter/espaço
+        for (char c : str.toCharArray()) {
+            int random = r.nextInt(chars.length());
+            codificada += c;
+            codificada += chars.charAt(random);
+        }
+        return codificada;
     }
 
     @Override
     public String decodifica(String str) {
-        return new StringBuilder(str).reverse().toString();
+        String decodificada = "";
+        // remove o caracter aleatorio de cada caracter
+        for (int i = 0; i < str.length(); i += 2) {
+            decodificada += str.charAt(i);
+        }
+        return decodificada;
     }
 
     @Override
