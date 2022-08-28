3f5c7e81f3fb29034d4b3b619c965f82a6646081
DuduPretto <eduardo.pretto@edu.pucrs.br>
2022-08-28 12:27:35 -0300

    adicionados comentarios no codigo

--- Codificadores/Codifica21103800.java
+++ Codificadores/Codifica21103800.java
@@ -19,10 +19,10 @@ public class Codifica21103800 implements Codifica{
         char aux[] = str.toCharArray();
         StringBuilder resultado = new StringBuilder();
         Random r = new Random();
-
         for (int i = 0; i < aux.length; i ++){
-            resultado.append(Character.toChars(aux[i] + 1));
-            if ((i % 3) == 0){
+            //adiciona 1 ao valor correspondente a cada letra da palavra na tabela ascii
+            resultado.append(Character.toChars(aux[i] + 1)); 
+            if ((i % 3) == 0){//apos cada posicao da string correspondente a um numero multiplo de tres, acrescenta tres caracteres aleatorios
                 for (int j = 0; j < 3; j++){
                     resultado.append(Character.toChars(r.nextInt(106) + 20));
                 }
@@ -38,8 +38,8 @@ public class Codifica21103800 implements Codifica{
         StringBuilder resultado = new StringBuilder();
         int i = 0;
         while (i<aux.length){
-            resultado.append(Character.toChars(aux[i] - 1));
-            if ((i % 3) == 0){
+            resultado.append(Character.toChars(aux[i] - 1));//diminui 1 ao valor correspondente a cada letra da palavra na tabela ascii
+            if ((i % 3) == 0){//ao encontrar um numero multiplo de tres pula as tres posicoes seguintes, que correspondem a caracteres aleatorios
                 i += 3;
             }
             i++;
