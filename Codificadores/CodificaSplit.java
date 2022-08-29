cf147155bc6058b15e23d2692d8d5b303966b751
Leonardo Forner <leonardoforner@gmail.com>
2022-08-29 15:47:22 -0300

    finalizado codifica

---
+++ Codificadores/CodificaSplit.java
@@ -0,0 +1,41 @@
+package Codificadores;
+
+public class CodificaSplit implements Codifica {
+
+    @Override
+    public String getNomeAutor() {
+
+        return "LeonardoLerschForner";
+    }
+
+    @Override
+    public String getMatriculaAutor() {
+
+        return "21100996-04";
+    }
+
+    @Override
+    public String codifica(String str) {
+        char[] string = str.toCharArray();
+        int ascii = 0;
+        int count = 0;
+        char ascii_c = 'a';
+
+        for (char s : string) {
+            ascii = (int) s;
+            ascii -= 4;
+            ascii_c = (char) ascii;
+            string[count] = ascii_c;
+            count++;
+
+        }
+        String codificado = new String(string);
+        return codificado;
+    }
+
+    @Override
+    public String decodifica(String str) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
